79d06167554281ca3da6abc6517ede539db84b7c
ZachC <ZachChristensen28@users.noreply.github.com>
2018-01-13 17:14:44 -0700

    first version of homework

--- CSIS_4300/Calculator/Calculator/ViewController.swift
+++ CSIS_4300/Calculator/Calculator/ViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    var result = Float()
+    var currentNumber = Float()
+    var currentOp = String()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currentOp = "="
+        resultsLabel.text = ("\(result)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func NumberPressed(_ sender: UIButton) {
+        currentNumber = currentNumber * 10 + Float(sender.titleLabel!.text!)!
+        resultsLabel.text = ("\(currentNumber)")
     }
-
-
+    
+    @IBAction func operationPressed(_ sender: UIButton) {
+        
+        switch currentOp {
+        case "=":
+            result = currentNumber
+        case "+":
+            result = result + currentNumber
+        case "-":
+            result = result - currentNumber
+        case "/":
+            result = result / currentNumber
+        case "*":
+            result = result * currentNumber
+        default:
+            print("what did you do?!")
+        }
+        
+        currentNumber = 0
+        resultsLabel.text = ("\(result)")
+        
+        if(sender.titleLabel!.text == "=") {
+            result = 0
+        }
+        
+        currentOp = sender.titleLabel!.text! as String
+    }
+    @IBAction func clearPressed(_ sender: UIButton) {
+        result = 0
+        currentNumber = 0
+        currentOp = "="
+        resultsLabel.text = ("\(result)")
+    }
 }
 
